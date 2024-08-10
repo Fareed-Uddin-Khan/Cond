532f48d29818a77eb8aac7cc0ac79438f32f0855
Fareed-Uddin-Khan <131321055+Fareed-Uddin-Khan@users.noreply.github.com>
2024-08-11 02:54:01 +0500

    commit2

---
+++ Cond.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Cond {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String choice;
+        do {
+        System.out.println("Enter Value of a");
+        int a;
+        int b;
+        while (true) {
+            if (scanner.hasNextInt()) {
+                a = scanner.nextInt();
+                break;
+            }
+            else {
+                System.out.println("The number entered is not an integer, please enter an integer");
+                scanner.next();
+            }
+                        
+        }
+        
+        System.out.println("Enter Value of b");
+        while (true) {
+            if (scanner.hasNextInt()) {
+                b = scanner.nextInt();
+                break;
+            }
+            else {
+                System.out.println("The number entered is not an integer, please enter an integer");
+                scanner.next();
+            }
+                        
+        }
+        
+        if ( a == b) {
+            System.out.println("a is equal to b");
+        }
+        else if (a > b) {
+            System.out.println("a is greater than b");
+        }
+        else {
+            System.out.println("a is smaller than b");
+        }
+        System.out.println("Do you want to check more numbers? Press Y to continue or any other key to exit.");
+        choice = scanner.next();
+    } while (choice.equalsIgnoreCase("Y"));
+        System.out.println("Exiting the program. Goodbye!");
+
+    }
+}
